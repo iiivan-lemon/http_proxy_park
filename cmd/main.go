0d75d30734455aa160f0f029a65129195aa1b840
iiivan-lemon <lipton2807@yandex.ru>
2022-12-21 16:06:49 +0300

    init work

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	server "example.com/m/internal/app/proxy"
+	"log"
+	"time"
+)
+
+func main() {
+	s := server.Server{
+		Addr:         "127.0.0.1:8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
